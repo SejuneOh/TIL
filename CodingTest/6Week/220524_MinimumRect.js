5d1256eaf1ac264792c66d0bd29e7208fe2d84fa
SejuneOh <etry0715@gmail.com>
2022-05-24 23:02:41 +0900

    docs: finish minimum rectangle

---
+++ CodingTest/6Week/220524_MinimumRect.js
@@ -0,0 +1,22 @@
+
+const size = [[60, 50], [30, 70], [60, 30], [80, 40]];
+
+function solution(sizes) {
+  let wmax = 0;
+  let hmax = 0;
+
+  const sortArr = sizes.map(arr => {
+    arr.sort((a, b) => a - b);
+
+    wmax = wmax > arr[0] ? wmax : arr[0];
+    hmax = hmax > arr[1] ? hmax : arr[1];
+
+    return [wmax, hmax]
+  });
+
+  return wmax * hmax;
+}
+
+
+console.log(solution(size));
+
